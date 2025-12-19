f3a28682b2ba54303bc26eb69b2e252c55b1bb07
rishittandon7 <rishit.tandon.7@gamil.com>
2025-12-19 12:51:30 +0530

    feat: Initialize Next.js project with Supabase, Tailwind CSS, and basic configuration.

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Fix for Supabase ESM import issues
   // serverExternalPackages: ['@supabase/supabase-js', '@supabase/ssr'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
